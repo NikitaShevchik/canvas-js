af736282557dba1002ff66c8948625810bd734d5
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-29 17:18:15 +0300

    canvas learn lines

--- js/scrypt.js
+++ js/scrypt.js
@@ -2,3 +2,46 @@
 
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
+
+function bird() {
+    ctx.beginPath();
+    ctx.moveTo(50, 100);
+    ctx.lineTo(100, 150);
+    ctx.lineTo(150, 100);
+    ctx.lineTo(50, 100);
+    ctx.stroke();
+}
+
+// Нарисуем квадратик
+
+function sqr() {
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(50, 150);
+    ctx.lineTo(150, 150);
+    ctx.lineTo(150, 50);
+    // ctx.lineTo(50, 50);
+    ctx.closePath(); // автоматически закроет квадратик
+    ctx.fill() // заполняет цветом
+    // ctx.stroke(); // делает контур
+}
+
+function practice() {
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(100, 100);
+    ctx.moveTo(50, 100);
+    ctx.lineTo(100, 50);
+    ctx.stroke(); // делает контур
+}
+
+function practice2() {
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(100, 100);
+    ctx.moveTo(50, 100);
+    ctx.lineTo(100, 50);
+    ctx.stroke(); // делает контур
+}
+practice2()
+
