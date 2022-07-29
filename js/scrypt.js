3b3bcba8156afdde779922d5451992c32f8fb2d4
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-29 17:34:54 +0300

    doneStrokeFillPractice

--- js/scrypt.js
+++ js/scrypt.js
@@ -38,10 +38,48 @@ function practice() {
 function practice2() {
     ctx.beginPath();
     ctx.moveTo(50, 50);
+    ctx.lineTo(50, 100);
     ctx.lineTo(100, 100);
-    ctx.moveTo(50, 100);
     ctx.lineTo(100, 50);
+    ctx.closePath()
     ctx.stroke(); // делает контур
 }
-practice2()
 
+function practice3() {
+    ctx.beginPath();
+    ctx.moveTo(50, 100);
+    ctx.lineTo(100, 70);
+    ctx.lineTo(150, 100);
+    ctx.closePath()
+    ctx.stroke(); // делает контур
+}
+
+function practice4() {
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(130, 30);
+    ctx.moveTo(150, 50);
+    ctx.lineTo(130, 70);
+    ctx.stroke(); // делает контур
+}
+
+function practice5() {
+    ctx.beginPath();
+    ctx.moveTo(50, 50);
+    ctx.lineTo(150, 50);
+    ctx.lineTo(150, 150);
+    ctx.lineTo(50, 150);
+    ctx.closePath()
+    ctx.fill()
+}
+
+function practice6() {
+    ctx.beginPath();
+    ctx.moveTo(50, 150);
+    ctx.lineTo(100, 50);
+    ctx.lineTo(150, 150);
+    ctx.closePath()
+    ctx.fill()
+}
+practice6()
